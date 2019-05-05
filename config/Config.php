5e05b7ba4a253abfaf3298440466acb42e0979bb
Rasul <super.karaev@list.ru>
2019-05-05 17:39:29 +0300

    config phpdev

--- config/Config.php
+++ config/Config.php
@@ -18,23 +18,3 @@ class Config
         }
     }
 }
-
-
-//class Config
-//{
-//    static  public function getConnectDb()
-//    {
-//        try
-//        {
-//            $host = 'parking.by';
-//            $dbname = 'rasulphpdev';
-//            $user = 'rasulkaraev';
-//            $password = '1999276776s';
-//            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user,$password);
-//
-//            return $db;
-//        } catch (PDOException $e) {
-//                exit($e->getMassage());
-//        }
-//    }
-//}
